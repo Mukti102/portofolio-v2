1f2e034395b1d7f79bde18337e00bef0783aa921
abdul mukti <abdulmukti40390@gmail.com>
2025-07-14 13:21:28 +0700

    delete assets on public

--- app/(projects)/Projects/layout.tsx
+++ app/(projects)/Projects/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import { Projects } from "@/data/projects/projects";
 
 function Layout({ children }: { children: React.ReactElement }) {
   return children;
